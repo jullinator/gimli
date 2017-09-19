00ad5abad59f7d62b1ced64e3eb6bff38d5df5ca
jullinator <justus.karlsson@hotmail.se>
2017-09-19 18:34:50 +0200

    asfsf

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-from nanpy import ArduinoApi
+from nanpy import ArduinoApi, SerialManager
 from dc import Motor, MotorController
 from servo import Servo, ServoController
 
-a = ArduinoApi()
+conn = SerialManager(device='/dev/ttyACM0')
+
+a = ArduinoApi(connection = conn)
 
 m1 = Motor(2,3,4,a)
 m2 = Motor(5,6,7,a)
 dc = MotorController(m1,m2)
 
-s1 = Servo(8)
-s2 = Servo(9)
+#s1 = Servo(8)
+#s2 = Servo(9)
 
-serv = ServoController(s1, s2)
+#serv = ServoController(s1, s2)
